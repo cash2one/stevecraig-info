7cf26f84b790d71a68dbc235b14b29de270dde60
Nick Pettazzoni <npettazz@akamai.com>
2015-08-28 13:31:26 -0400

    log errors in basehandler

--- event_handlers/basehandler.py
+++ event_handlers/basehandler.py
@@ -1,10 +1,20 @@
 from tornado.web import RequestHandler
+from traceback import format_exception
 import logging
 
 class BaseHandler(RequestHandler):
 
     def __init__(self, application, request, **kwargs):
         self.logger = logging.getLogger(__name__)
-        self.logger.addHandler(logging.StreamHandler())
 
         super(BaseHandler, self).__init__(application, request, **kwargs)
+
+
+    def write_error(self, status_code, **kwargs):
+        if 'exc_info' in kwargs:
+            etype, value, tb = kwargs['exc_info']
+            trace = ''.join(format_exception(etype, value, tb, 10)) 
+        else:
+            trace = "None"
+        self.logger.error('%s: %s' % (status_code, trace))
+        self.write('you done fucked up: %s' % status_code)
--- event_handlers/namechange_handler.py
+++ event_handlers/namechange_handler.py
@@ -3,10 +3,6 @@ from basehandler import BaseHandler
 class NamechangeEventHandler(BaseHandler):
     BUTL_EVENT_API_PATTERNS = ['namechange']
 
-    def write_error(self, status_code, **kwargs):
-        self.logger.error('butts')
-        self.write('you done fucked up')
-
     def post(self):
         test = self.get_argument("test", default = None)
         print test
